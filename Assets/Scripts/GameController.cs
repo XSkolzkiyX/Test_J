f3a832be052b0d53bd09e5b4e2d048c1c2148456
XSkolzkiyX <m.polyakov2013@gmail.com>
2022-02-11 09:16:06 +0200

    Update GameController.cs

--- Assets/Scripts/GameController.cs
+++ Assets/Scripts/GameController.cs
@@ -18,28 +18,6 @@ public class GameController : MonoBehaviour
     {
         for (int i = 0; i < count; i++)
         {
-            /*float potentionalX = Random.Range(-5, 5);
-            foreach(float X in positionsX)
-            {
-                do
-                {
-                    potentionalX = Random.Range(-5, 5);
-                }
-                while (potentionalX != X);
-                x = potentionalX;
-            }
-            positionsX.Concat(new float[] { x }).ToArray();
-            float potentionalZ = Random.Range(-4, 4);
-            foreach (float Z in positionsZ)
-            {
-                do
-                {
-                    potentionalZ = Random.Range(-4, 4);
-                }
-                while (potentionalZ != Z);
-                z = potentionalX;
-            }
-            positionsZ.Concat(new float[] { z }).ToArray();*/
             //x = GeneratePotentional(5, positionsX);
             //z = GeneratePotentional(4, positionsZ);
 
